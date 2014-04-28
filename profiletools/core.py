d00c0eb36897244c78e1e3c8c4b07a2757f92d6c
markchil <mark.chilenski@gmail.com>
2014-04-28 17:09:25 -0400

    Completed constructor for Profile.

--- profiletools/core.py
+++ profiletools/core.py
@@ -22,4 +22,39 @@ from __future__ import division
 
 class Profile(object):
     """Object to abstractly represent a profile.
+    
+    Parameters
+    ----------
+    X_dim : positive int, optional
+        Number of dimensions of the independent variable. Default value is 1.
+    X_units : str, list of str or None, optional
+        Units for each of the independent variables. If `X_dim`=1, this should
+        given as a single string, if `X_dim`>1, this should be given as a list
+        of strings of length `X_dim`. Default value is `None`, meaning a list
+        of empty strings will be used.
+    y_units : str, optional
+        Units for the dependent variable. Default is an empty string.
+    
+    Attributes
+    ----------
+    X_dim : positive int
+        The number of dimensions of the independent variable.
+    X_units : list of str, (X_dim,)
+        The units for each of the independent variables.
+    y_units : str
+        The units for the dependent variable.
     """
+    def __init__(self, X_dim=1, X_units=None, y_units=''):
+        self.X_dim = X_dim
+        if X_units is None:
+            X_units = [''] * X_dim
+        elif X_dim == 1:
+            X_units = [X_units]
+        elif len(X_units) != X_dim:
+            raise ValueError("The length of X_units must be equal to X_dim!")
+                
+        self.X_units = X_units
+        self.y_units = y_units
+    
+    def add_data(self, X, y, err_X=None, err_Y=0):
+        
